d402cf6062e43c31288d1ed32f9daef55ce9404b
André Lademann <vergissberlin@googlemail.com>
2019-11-18 23:47:39 +0100

    Set some rules for tslint

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import express from "express";
-const app = express();
-const port = 8080; // default port to listen
+import express from 'express'
+const app = express()
+const port = 8080 // default port to listen
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.json({nothing: "here"});
-} );
-app.get( "/ticket/create", ( req, res ) => {
-    res.json({nothing: "here"});
-});
+app.get( '/', ( req, res ) => {
+		res.json({nothing: 'here'})
+} )
+app.get( '/ticket/create', ( req, res ) => {
+		res.json({nothing: 'here'})
+})
 
 // start the Express server
 app.listen( port, () => {
-    // tslint:disable-next-line:no-console
-    console.log( `server started at http://0.0.0.0:${ port }` );
-});
+		// tslint:disable-next-line:no-console
+		console.log( `server started at http://0.0.0.0:${ port }` )
+})
