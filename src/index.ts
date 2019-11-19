ad53c2fe1dc2673f5110a7804da0e973eb65ba1f
Andre Lademann <andre.lademann@netresearch.de>
2019-11-19 17:15:34 +0100

    Use middleware later

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,22 @@ const PORT = Number(process.env.PORT) || 8080
 const HOST = '0.0.0.0'
 
 const loggerMiddleware = (req: express.Request, res: express.Response, next: () => void) => {
-  	// tslint:disable-next-line:no-console
-	console.log(`::: REQUEST ${req.method} ${req.path}`);
+	// tslint:disable-next-line:no-console
+	console.log(`::: REQUEST ${req.method} ${req.path}`)
+	// tslint:disable-next-line:no-console
+	console.log(req.body)
+	// tslint:disable-next-line:no-console
+	console.log(req.params)
+	// tslint:disable-next-line:no-console
+	console.log(req.query)
 
 	res.json({
 		body: req.body,
 		params: req.params,
 		query: req.query
 	})
-  next();
+	next()
 }
-
-app.use(loggerMiddleware);
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -25,17 +29,20 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+// use logger
+app.use(loggerMiddleware)
+
 app.all( '*', ( req, res ) => {
-  	// tslint:disable-next-line:no-console
-	  console.log(req.body)
-	  // tslint:disable-next-line:no-console
-	  console.log(req.params)
-	  // tslint:disable-next-line:no-console
-	  console.log(req.query)
+	// tslint:disable-next-line:no-console
+	console.log(req.body)
+	// tslint:disable-next-line:no-console
+	console.log(req.params)
+	// tslint:disable-next-line:no-console
+	console.log(req.query)
 })
 
 // start the Express server
 app.listen( PORT, HOST, () => {
-		// tslint:disable-next-line:no-console
-		console.log( `server started at http://0.0.0.0:${ PORT }` )
+	// tslint:disable-next-line:no-console
+	console.log( `server started at http://0.0.0.0:${ PORT }` )
 })
