43b1b24c3102f2ea2fcb0b10788b396995ec4bd8
Andre Lademann <andre.lademann@netresearch.de>
2019-11-19 15:39:31 +0100

    Add body parser

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
+import bodyParser from 'body-parser'
 import express from 'express'
+
 const app = express()
-const PORT = 8080
+const PORT = 8090
 const HOST = '0.0.0.0'
 
-// define a route handler for the default home page
-app.get( '/', ( req, res ) => {
-		res.json({nothing: 'here'})
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.all( '/', ( req, res ) => {
+	// tslint:disable-next-line:no-console
+	console.log(req.body)
+	// tslint:disable-next-line:no-console
+	console.log(req.params)
+	// tslint:disable-next-line:no-console
+	console.log(req.query)
+
+	res.json({
+		body: req.body,
+		params: req.params,
+		query: req.query
+	})
 } )
 
 app.get( '/ticket/create', ( req, res ) => {
